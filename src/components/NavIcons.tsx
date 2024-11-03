451802da33e6d27e90296aaa51d692bf01995a04
Tejas Dabholkar <tejasdabholkar@elliotsystems.com>
2024-11-03 21:44:40 +0530

    cart logo click bug fix

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -46,13 +46,15 @@ const NavIcons = () => {
                 className="cursor-pointer"
             />
 
-            <div className="relative cursor-pointer">
+            <div
+                className="relative cursor-pointer"
+                onClick={() => setIsCartOpen((prev) => !prev)}
+            >
                 <Image
                     src="/cart.png"
                     alt=""
                     width={22}
                     height={22}
-                    onClick={() => setIsCartOpen((prev) => !prev)}
                 />
                 <div className="absolute -top-4 -right-4 w-6 h-6 bg-alertMsg rounded-full text-white text-sm flex items-center justify-center">2</div>
             </div>
